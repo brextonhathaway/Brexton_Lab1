65c46dfb287b2d68c0534c73f1aac11343ed9a8b
Brexton Hathaway <bhathaw1@aum.edu>
2026-04-06 10:28:06 -0500

    Added second output line

--- Brexton_Lab1/Brexton_Lab1.cpp
+++ Brexton_Lab1/Brexton_Lab1.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello from Brexton Hathaway! This is my first Git commit.\n";
+    cout << "Hello from Brexton Hathaway! This is my first Git commit.\n";
+    cout << "Git makes collaboration easy!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
