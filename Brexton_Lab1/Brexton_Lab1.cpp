725061f0158d2af05574e1888de0c388205a3a99
Brexton Hathaway <bhathaw1@aum.edu>
2026-04-06 11:17:51 -0500

    Final mini-task update

--- Brexton_Lab1/Brexton_Lab1.cpp
+++ Brexton_Lab1/Brexton_Lab1.cpp
@@ -8,18 +8,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello from Brexton Hathaway! This is my first Git commit.\n";
-    cout << "Git makes collaboration easy!\n";
+    cout << "Hello from Brexton Hathaway! This is my first Git commit." << endl;
+    cout << "Git makes collaboration easy!" << endl;
     cout << "Changes made by Samuel!" << endl;
+    cout << "Lab 1 completed on April 6th, 2026!" << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
